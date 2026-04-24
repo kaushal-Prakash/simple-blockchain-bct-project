fc328e68ca3d5563fb50f2416f584d840468b1ce
kaushal-Prakash <kingkaushal1289@gmail.com>
2026-04-24 12:37:18 +0530

    added Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        Blockchain blockchain = new Blockchain();
+
+        blockchain.addBlock("Transaction 1: A pays B 100");
+        blockchain.addBlock("Transaction 2: B pays C 50");
+        blockchain.addBlock("Transaction 3: C pays D 25");
+
+        blockchain.printChain();
+
+        System.out.println("Is Blockchain valid? " + blockchain.isChainValid());
     }
 }
